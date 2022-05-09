70fc3d24f8da267ba92733300705028efb0d828e
Ana Paula Buchholz <anapbuchholz@gmail.com>
2022-05-09 20:30:50 -0300

    Adicionando projeto MVC

---
+++ src/LimpouFaxinas/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LimpouFaxinas.Models
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+    {
+
+    }
+}
