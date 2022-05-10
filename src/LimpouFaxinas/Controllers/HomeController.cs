098082fe79c31b1af03afd0faa12bf5d15cda692
Ana Paula Buchholz <anapbuchholz@gmail.com>
2022-05-09 21:21:09 -0300

    fazendo alterações no HTML + MVC

--- src/LimpouFaxinas/Controllers/HomeController.cs
+++ src/LimpouFaxinas/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using LimpouFaxinas.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LimpouFaxinas.Controllers
 {
@@ -19,11 +15,6 @@ namespace LimpouFaxinas.Controllers
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
--- src/LimpouFaxinas/Models/ApplicationDbContext.cs
+++ src/LimpouFaxinas/Models/ApplicationDbContext.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace LimpouFaxinas.Models
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
-    {
+//namespace LimpouFaxinas.Models
+//{
+//    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+//    {
 
-    }
-}
+//    }
+//}
