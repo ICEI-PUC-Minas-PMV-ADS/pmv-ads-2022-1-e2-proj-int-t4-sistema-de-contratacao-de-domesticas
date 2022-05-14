e4d3d3fa1b8cd8772f64aee678dbb08e38ffded4
Tiago <tiago.rodrigues.1338413@sga.pucminas.br>
2022-05-14 19:07:25 -0300

    Retornando_aos_ajustes_iniciais

---
+++ src/Models/Serviço.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LimpouFaxinas.Models
+
+{
+    [Table("Serviço")]
+    public class Servico
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obritarório informar o nome!")]
+        public string TipodeFaxina{ get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o tipo de serviço")]
+    }
+}
