4a6ebc35913e851d69cb1e2ec15588c1669df0af
TiagoMiquéias <tiagomiqueiasrodrigues@gmail.com>
2022-05-25 00:46:06 -0300

    Alterações_nas_tabelas

--- Limpou_Faxinas/Models/Servico.cs
+++ Limpou_Faxinas/Models/Servico.cs
@@ -13,9 +13,6 @@ namespace Limpou_Faxinas.Models
         [Required(ErrorMessage = "Obrigatório descrever a data do serviço ")]
         public DateTime DataHora { get; set; }
 
-        [Required(ErrorMessage = "Obrigatório descrever o preço do serviço ")]
-        public Decimal Preco { get; set; }
-
         [Required(ErrorMessage = "Obrigatório agendar o serviço ")]
         public Boolean Agendar { get; set; }
 
@@ -27,7 +24,18 @@ namespace Limpou_Faxinas.Models
         [MaxLength(200, ErrorMessage = "Descrição não pode exceder {200} caracteres")]
         public string Descricao { get; set; }
 
-        [NotMapped]
+        [Required(ErrorMessage = "Informe o preço do serviço")]
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(10,2")]
+        //Para definir o valor mínimo e máximo
+        [Range(100,300, ErrorMessage = "O preço deve estar entre 100 e 300 reais")]
+        public Decimal Preco { get; set; }
+
+        public string ImagemUrl { get; set; }
+
+
+        public bool DataDisponibilidade { get; set; }
+                    
         public List<Usuario> Usuarios { get; set; }
 
         
--- Limpou_Faxinas/Models/Usuario.cs
+++ Limpou_Faxinas/Models/Usuario.cs
@@ -3,11 +3,24 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Limpou_Faxinas.Models
 {
+    [Table("Usuários")]
     public class Usuario
     {
+        [Key]
         public int UsuarioId { get; set; }
+
+        [StringLength(70, ErrorMessage = "O tamanho máximo é de 70 caracteres")]
+        [Required(ErrorMessage = "Informe o nome do usuário")]
         public string Nome { get; set; }
+
+        [StringLength(100, ErrorMessage = "O tamanho máximo é de 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome do usuário")]
         public string Endereco { get; set; }
+
+        public int CPF { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho máximo é de 200 caracteres")]
+        [Required(ErrorMessage = "Informe o nome do usuário")]
         public int Avaliacao { get; set; }
 
 
