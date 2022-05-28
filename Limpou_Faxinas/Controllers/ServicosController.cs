294371fb2af9cd04d48d2917a4f1522a2cc6141e
TiagoMiquéias <tiagomiqueiasrodrigues@gmail.com>
2022-05-27 23:48:54 -0300

    Adicionando-Controller

---
+++ Limpou_Faxinas/Controllers/ServicosController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Limpou_Faxinas.Context;
+using Limpou_Faxinas.Models;
+
+namespace Limpou_Faxinas.Controllers
+{
+    public class ServicosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ServicosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Servicos
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Servicos.ToListAsync());
+        }
+
+        // GET: Servicos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Servicos == null)
+            {
+                return NotFound();
+            }
+
+            var servico = await _context.Servicos
+                .FirstOrDefaultAsync(m => m.ServicoId == id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+
+            return View(servico);
+        }
+
+        // GET: Servicos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Servicos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ServicoId,tipo_de_servico,Descricao,Preco")] Servico servico)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(servico);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servico);
+        }
+
+        // GET: Servicos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Servicos == null)
+            {
+                return NotFound();
+            }
+
+            var servico = await _context.Servicos.FindAsync(id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+            return View(servico);
+        }
+
+        // POST: Servicos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ServicoId,tipo_de_servico,Descricao,Preco")] Servico servico)
+        {
+            if (id != servico.ServicoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(servico);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServicoExists(servico.ServicoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servico);
+        }
+
+        // GET: Servicos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Servicos == null)
+            {
+                return NotFound();
+            }
+
+            var servico = await _context.Servicos
+                .FirstOrDefaultAsync(m => m.ServicoId == id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+
+            return View(servico);
+        }
+
+        // POST: Servicos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Servicos == null)
+            {
+                return Problem("Entity set 'AppDbContext.Servicos'  is null.");
+            }
+            var servico = await _context.Servicos.FindAsync(id);
+            if (servico != null)
+            {
+                _context.Servicos.Remove(servico);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServicoExists(int id)
+        {
+          return _context.Servicos.Any(e => e.ServicoId == id);
+        }
+    }
+}
