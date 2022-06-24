b72650f5dd2bf2af75a7734c2df28bc8a5c80c8b
Filipe <filipemarquespascoal@hotmail.com>
2022-06-24 00:07:31 -0300

    Criação de novas telas e atualização no front

--- Limpou_Faxinas/Controllers/HomeController.cs
+++ Limpou_Faxinas/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace Limpou_Faxinas.Controllers
             return View();
         }
 
+        public IActionResult OpcaoDeCadastros()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
